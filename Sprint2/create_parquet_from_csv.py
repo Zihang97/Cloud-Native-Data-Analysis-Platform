a3d9ac43e853eaef2b206da89682d6ae6979ba44
Zihang97 <jzh15@bu.edu>
2021-10-20 15:03:18 -0400

    update parquet scripts

--- Sprint2/create_parquet_from_csv.py
+++ Sprint2/create_parquet_from_csv.py
@@ -23,25 +23,25 @@ def read_csv(filename):
     )
 dfs = list(map(read_csv, files))
 df = pd.concat(dfs)
-# df.to_parquet("./turnstile_parquet/turnstile.parquet")
-df.to_csv("turnstile_15_to_21.csv", index=False)
+df.to_parquet("./turnstile_parquet/turnstile.parquet")
+# df.to_csv("turnstile_15_to_21.csv", index=False)
 
 
 # Method two: convert dataframe to pyarrow table, then using ParquetWriter convert table to parquet
 # this method can manually control row group number and content
-# years = range(15,22)
+years = range(15,22)
 
-# def read_csv_by_year(year):
-#     files_by_year = glob.glob(f"./turnstile_csv/turnstile{year}*.csv")
-#     dfs = list(map(read_csv, files_by_year))
-#     return pd.concat(dfs)
+def read_csv_by_year(year):
+    files_by_year = glob.glob(f"./turnstile_csv/turnstile{year}*.csv")
+    dfs = list(map(read_csv, files_by_year))
+    return pd.concat(dfs)
 
-# dfs = list(map(read_csv_by_year, years))
+dfs = list(map(read_csv_by_year, years))
 
-# table = pa.Table.from_pandas(dfs[0], preserve_index=False)
-# writer = pq.ParquetWriter('./turnstile_parquet/turnstile_by_year.parquet', table.schema)
+table = pa.Table.from_pandas(dfs[0], preserve_index=False)
+writer = pq.ParquetWriter('./turnstile_parquet/turnstile_by_year.parquet', table.schema)
 
-# for df in dfs:
-#     table = pa.Table.from_pandas(df, preserve_index=False)
-#     writer.write_table(table)
-# writer.close()
+for df in dfs:
+    table = pa.Table.from_pandas(df, preserve_index=False)
+    writer.write_table(table)
+writer.close()
