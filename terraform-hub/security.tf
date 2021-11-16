8dc679eee0ca7bd9460d73164d735d23caf7eb8a
Ibrahim Chand <ichand@bu.edu>
2021-11-16 09:00:34 -0500

    Security resources

--- terraform-hub/security.tf
+++ terraform-hub/security.tf
@@ -1,3 +1,43 @@
 resource "aws_security_group" "hub" {
-    name = "${var.se
+    name = "${var.service}-${uuid()}"
+	description = "Security group for ${title(var.service)}"
+
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "SSH"
+	}
+
+	ingress {
+		from_port = 8888
+		to_port = 8898
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		description = "${title(var.service)}"
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_block = ["0.0.0.0/0"]
+	}
+}
+
+resource "tls_private_key" "key" {
+	algorithm = "RSA"
+	rsa_bits = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+	key_name = "key-${uuid}"
+	public_key = "${tls_private_key.key.public_key_openssh}"
+}
+
+resource "local_file" "pem" {
+	filename = "${aws_key_pair.generated_key.key_name}.pem"
+	content = "${tls_private_key.key.private_key_pem}"
+	file_permission = "400"
 }
