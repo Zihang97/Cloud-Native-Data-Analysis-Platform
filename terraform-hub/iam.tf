313490fc2a6c438d3815433e12ec2a31f762031c
ibchand <ichand@bu.edu>
2021-11-16 20:03:07 -0500

    Added IAM Roles

---
+++ terraform-hub/iam.tf
@@ -0,0 +1,42 @@
+resource "aws_iam_role" "user_role" {
+    name = "user_role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Sid = ""
+            Principal = {
+                Service = "ec2.amazonaws.com"
+            }
+        }]
+    })
+}
+
+resource "aws_iam_role_policy" "user_role_policy" {
+    name = "user_role_policy"
+    role = "${aws_iam_role.user_role.id}" 
+    policy = jsonencode({
+        Version = "2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Action = ["s3:*"]
+                Resource = "arn:aws:s3:::528-user-bucket"
+            },
+            {
+                Effect = "Allow"
+                Action = [
+                    "s3:ListBucket",
+                    "s3:GetObject",
+                ]
+                Resource = "arn:aws:s3:::528-central-bucket"
+            }
+        ]
+    })
+}
+
+resource "aws_iam_instance_profile" "instance_profile" {
+    name = "instance_profile"
+    role = "${aws_iam_role.user_role.name}"
+}
---
+++ terraform-hub/var.tf
@@ -0,0 +1,11 @@
+variable "availability_zone" {
+    default = "us-east-2a"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "service" {
+    default = "hub_instance"
+}
