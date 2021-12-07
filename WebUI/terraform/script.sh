b1d7906b4b585b550a62acc3419446fd21863a5b
Ibrahim Chand <ib@ibrahims-mbp.myfiosgateway.com>
2021-12-07 03:51:08 -0500

    Added reference

--- WebUI/terraform/script.sh
+++ WebUI/terraform/script.sh
@@ -33,5 +33,5 @@ runuser -l ec2-user -c 'jupyter notebook --generate-config' &&
     sed -i -e "s/#c.NotebookApp.ip = 'localhost'/c.NotebookApp.ip = '"$(curl http://169.254.169.254/latest/meta-data/public-hostname)"'/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py &&
     sed -i -e "s/#c.NotebookApp.allow_origin = ''/c.NotebookApp.allow_origin = '*'/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py &&
     sed -i -e "s/#c.NotebookApp.open_browser = True/c.NotebookApp.open_browser = False/g" /home/ec2-user/.jupyter/jupyter_notebook_config.py
-
+# Reference: https://github.com/wblakecannon/terraform-jupyter
 
