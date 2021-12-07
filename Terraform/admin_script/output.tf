238fcc08a30d50bbd16ca4bd7eee37b6b0c439c2
Ibrahim Chand <ib@macbook-pro.myfiosgateway.com>
2021-12-07 12:26:07 -0500

    Change output syntax

--- Terraform/admin_script/output.tf
+++ Terraform/admin_script/output.tf
@@ -1,8 +1,8 @@
-output "SSH via Key" {
+output "SSH_via_Key" {
   value = "ssh -i ${aws_key_pair.generated_key.key_name}.pem ec2-user@${aws_instance.central_instance.public_dns}"
 }
 
-output "SSH via UNIX User" {
+output "SSH_via_UNIX_User" {
   value = "ssh ${var.username}@${aws_instance.central_instance.public_dns}"
 }
 
