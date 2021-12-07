16eaa1898f8671b823db4bc0331451330fc1a105
Ibrahim Chand <ib@macbook-pro.myfiosgateway.com>
2021-12-07 02:11:56 -0500

    Fixed output files

--- Terraform/admin_script/output.tf
+++ Terraform/admin_script/output.tf
@@ -1,5 +1,9 @@
-output "conn" {
-  value = "ssh -i \"${aws_key_pair.generated_key.key_name}.pem\" ec2-user@${aws_instance.central_instance.public_dns}"
+output "SSH via Key" {
+  value = "ssh -i ${aws_key_pair.generated_key.key_name}.pem ec2-user@${aws_instance.central_instance.public_dns}"
+}
+
+output "SSH via UNIX User" {
+  value = "ssh ${var.username}@${aws_instance.central_instance.public_dns}"
 }
 
 resource "local_file" "output" {
--- Terraform/user_script/output.tf
+++ Terraform/user_script/output.tf
@@ -1,5 +1,5 @@
 output "SSH_via_Key" {
-  value = "ssh -i \"${aws_key_pair.generated_key.key_name}.pem\" ec2-user@${aws_instance.hub_instance.public_dns}"
+  value = "ssh -i ${aws_key_pair.generated_key.key_name}.pem ec2-user@${aws_instance.hub_instance.public_dns}"
 }
 
 output "SSH_via_UNIX_User" {
