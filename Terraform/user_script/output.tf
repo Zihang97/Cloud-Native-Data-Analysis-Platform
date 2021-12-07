b1f213bcc171c573bba7a8a5a257a5ecffcd615d
Ibrahim Chand <ib@macbook-pro.myfiosgateway.com>
2021-12-07 02:08:38 -0500

    Fixed Jupyter

--- Terraform/user_script/output.tf
+++ Terraform/user_script/output.tf
@@ -1,5 +1,9 @@
-output "conn" {
+output "SSH_via_Key" {
   value = "ssh -i \"${aws_key_pair.generated_key.key_name}.pem\" ec2-user@${aws_instance.hub_instance.public_dns}"
+}
+
+output "SSH_via_UNIX_User" {
+  value = "ssh ${var.username}@${aws_instance.hub_instance.public_dns}"
 }
 
 resource "local_file" "output" {
--- Terraform/user_script/script.sh
+++ Terraform/user_script/script.sh
@@ -8,6 +8,13 @@ sleep 1m
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/home/ec2-user/terraform.log 2>&1
+# Create Unix User
+sudo useradd -s /bin/bash -m -d /home/admin -g root user
+echo "user:password1" | chpasswd
+echo 'user ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
+sudo sed -i '61s/.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i '63s/.*/#PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
 # Update AL2
 sudo yum update -y
 # Mount /anaconda3
@@ -16,13 +23,6 @@ sudo mkdir /anaconda3
 sudo mount /dev/sdb /anaconda3
 sudo chown -R ec2-user:ec2-user /anaconda3
 sudo echo "UUID=$(lsblk -nr -o UUID,MOUNTPOINT | grep "/anaconda3" | cut -d ' ' -f 1) /anaconda3 xfs defaults,nofail 1 2" >> /etc/fstab
-# Create Unix User
-sudo useradd -s /bin/bash -m -d /home/user -g root user
-echo "user:password1" | chpasswd
-echo 'user ALL=(ALL:ALL) ALL' | sudo EDITOR='tee -a' visudo
-sudo sed -i '61s/.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sudo sed -i '63s/.*/#PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo systemctl restart sshd.service
 # Install Anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh -O /home/ec2-user/anaconda.sh &&
     bash /home/ec2-user/anaconda.sh -u -b -p /anaconda3 &&
